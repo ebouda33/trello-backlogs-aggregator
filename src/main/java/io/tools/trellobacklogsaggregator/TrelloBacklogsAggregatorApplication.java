291918ea3e4b2d081d00b6ad5fc3e74969265d04
Mickaël PONSOLLE <mickael.ponsolle@cpam-bordeaux.cnamts.fr>
2017-05-22 15:51:12 +0200

    Reinitilisation with SpringBoot(Thymeleaf)

---
+++ src/main/java/io/tools/trellobacklogsaggregator/TrelloBacklogsAggregatorApplication.java
@@ -0,0 +1,12 @@
+package io.tools.trellobacklogsaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloBacklogsAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelloBacklogsAggregatorApplication.class, args);
+	}
+}
