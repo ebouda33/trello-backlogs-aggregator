39e786cbe0f9f66a140894ed20498f8762d1917a
Mickaël PONSOLLE <mickael.ponsolle@gmail.com>
2017-05-27 09:29:26 +0200

    Sort sprint columns

--- src/main/java/io/tools/trellobacklogsaggregator/controllers/SprintController.java
+++ src/main/java/io/tools/trellobacklogsaggregator/controllers/SprintController.java
@@ -1,10 +1,15 @@
 package io.tools.trellobacklogsaggregator.controllers;
 
+import java.util.Comparator;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import io.tools.trellobacklogsaggregator.configuration.CustomConfiguration;
+import io.tools.trellobacklogsaggregator.model.Column;
+import io.tools.trellobacklogsaggregator.model.Sprint;
 import io.tools.trellobacklogsaggregator.repository.BacklogsRepository;
 
 @Controller
@@ -13,10 +18,26 @@ public class SprintController {
     @Autowired
     private BacklogsRepository backlogsRepository;
 
+    @Autowired
+    private CustomConfiguration customConfiguration;
+
     @RequestMapping("/sprint")
     public String index(Model model) {
         if (backlogsRepository.read() != null) {
-            model.addAttribute("sprint", backlogsRepository.read().getSprint());
+            Sprint sprint = backlogsRepository.read().getSprint();
+            sprint.getColumns().sort(new Comparator<Column>() {
+
+                @Override
+                public int compare(Column col1, Column col2) {
+                    int indexCol1 = customConfiguration.getColumnInSprintAllowed()
+                            .indexOf(col1.getName().toLowerCase());
+                    int indexCol2 = customConfiguration.getColumnInSprintAllowed()
+                            .indexOf(col2.getName().toLowerCase());
+                    return indexCol1 - indexCol2;
+                }
+
+            });
+            model.addAttribute("sprint", sprint);
         }
         return "sprint";
     }
