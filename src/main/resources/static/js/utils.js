70765c4fff8f472f4b7fa02f98a61aefc390f03e
eric <eric@aboudaram.net>
2019-10-07 09:25:43 +0200

    Evolution Bouton batch interface copie du sprint pour excel

--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -4,6 +4,9 @@ jQuery["postJSON"] = function (url, data, success, failure,dataType) {
 jQuery["putJSON"] = function (url, data, success, failure, dataType) {
     return ajax(url,"PUT", data,success,failure,dataType);
 };
+jQuery["getJSON"] = function (url, data, success, failure, dataType) {
+    return ajax(url,"GET", data,success,failure,dataType);
+};
 
 function ajax(url,method, data, success, failure, dataType){
     if (jQuery.isFunction(data)) {
